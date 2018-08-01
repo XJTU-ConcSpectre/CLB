072cb3edd8cb44c03bd4549d0fe8ac31d304323b
Hao Yu <1129719536@qq.com>
2018-08-01 23:08:54 +0800

    ...

--- 4-other/zip.py
+++ 4-other/zip.py
@@ -21,6 +21,7 @@ for parent, dirnames, filenames in os.walk(rootdir):
     for filename in filenames:
         if "makefile" == filename:
             print(os.path.join(parent, filename))
+            os.chdir(parent)
             os.system('make')
 
 for parent, dirnames, filenames in os.walk(rootdir):
@@ -30,7 +31,8 @@ for parent, dirnames, filenames in os.walk(rootdir):
             shutil.rmtree(os.path.join(parent, dirname))
         if "programme" == dirname:
             print(os.path.join(parent, dirname))
-            os.remove(os.path.join(parent, dirname+".zip"))
+            if os.path.exists(os.path.join(parent, dirname+".zip"))
+                os.remove(os.path.join(parent, dirname+".zip"))
             make_zip(os.path.join(parent, dirname),os.path.join(parent, dirname+".zip"))
             
 
